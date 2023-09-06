21573a1432d6e8091fed86b8b457ce2f5f8ac309
Lee <leevashan.reddi@aax.co.za>
2023-09-06 22:14:22 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { debounceTime, takeUntil } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, throwError } from "rxjs";
+import { map, catchError } from 'rxjs/operators';
+import { Subject } from 'rxjs/internal/Subject';
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mtg-decker';
+export class AppComponent implements OnInit, OnDestroy {
+
+  public cards$: Observable<any[]>;
+  public searchField = new FormControl();
+  public subscription$ = new Subject();
+
+  public list = [];
+
+  constructor(private _http: HttpClient) { }
+
+  ngOnDestroy() {
+    this.subscription$.next();
+    this.subscription$.complete();
+  }
+
+  ngOnInit(): void {
+    this.searchField.valueChanges
+      .pipe(takeUntil(this.subscription$), debounceTime(500))
+      .subscribe(text => {
+        console.log(text);
+
+        if (typeof text === "string") {
+          this.getCard(text);
+        }
+
+        if (typeof text === 'object') {
+          // add to list
+          this.list.push(text);
+          this.searchField.setValue("", { emitEvent: false });
+        }
+      });
+  }
+
+  public getCard(name: string): void {
+    const url = `https://api.magicthegathering.io/v1/cards?name=${name}&page=1&pageSize=5`;
+    this.cards$ = this._http.get<any>(url)
+      .pipe(
+        map((res) => {
+          const { cards } = res;
+          return cards;
+        }),
+        catchError(res => {
+          return throwError(res);
+        }));
+  }
+
+  public selected(event: any): void {
+    console.log("selected", event);
+  }
+
+  public displayFn(card: any): string {
+    return card && card.name ? card.name : '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    MatInputModule,
+    AppRoutingModule,
+    MatListModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
